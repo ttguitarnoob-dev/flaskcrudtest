46670a4ab1c67b76ea8aa2c4ec7edb188ee1926c
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-05-31 12:05:19 -0500

    initiated database

--- application/app.py
+++ application/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 
 #initialize app
 app = Flask(__name__)
 
+#Set configs
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:/// database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Initiate database object
+db = SQLAlchemy(app)
+
+#Create Database
+class TodoList(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    description = db.Column(db.String(400), nullable = False)
 
 
 
