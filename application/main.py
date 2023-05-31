5de7d56de7b0d9d445f057a324cf4bbb8a2e8229
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-05-31 11:03:41 -0500

    created database instances, basic flask app, and schema

---
+++ application/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from datetime import datetime
+
+#Flask app initialize
+app = Flask(__name__)
+
+#Set configs
+app.config['SQLALCHEMY_DATABASE_URI'] = "SQLITE:/// database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Initialize dadabase object
+db = SQLAlchemy(app)
+
+#Create Marshmallow Object
+ma = Marshmallow(app)
+
+#Database Create
+class TodoList(db.Model):
+    id = db.Columns(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    description = db.Column(db.String(400), nullable = False)
+    completed = db.Column(db.Boolean, nullable = False, default = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return self.id
+    
+#Schema
+class TodoListSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'description', 'completed', 'date_created')
+
+#Routes
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
